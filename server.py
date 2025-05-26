81f0445d398a5da3fecc0d38f3b13cca20ae497d
lakshay102 <lakshayanil165@gmail.com>
2025-05-26 22:06:41 +0530

    creating web server

---
+++ server.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    '''Handle HTTP requests by returning a fixed 'page'.'''
+
+    # Page to send back.
+    Page = '''\
+<html>
+<body>
+<p>Hello, web!</p>
+</body>
+</html>
+'''
+
+    # Handle a GET request.
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page.encode('utf-8'))))
+        self.end_headers()
+        self.wfile.write(self.Page.encode('utf-8'))
+
+#-----------------------------------------------------------------------
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
